fa9997d978e8f2402469e52064cd8463e1746b90
Sulistyo <sulistyoardani@gmail.com>
2023-09-16 16:31:05 +0700

    fix: structure table

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
- // eslint-disable-next-line react/prop-types
+// eslint-disable-next-line react/prop-types
 import React from 'react'
 import { useState } from 'react'
 
@@ -11,30 +11,26 @@ function App() {
   const StatisticLine = (props) => {
     return (
       <>
-        <table>
-          <td>
-            <tr>{props.text}</tr>
-          </td>
-          <td>
-            <tr>{ props.value}</tr>
-          </td>
-        </table>
+        <tr>
+          <td>{props.text}</td>
+          <td>{props.value}</td>
+        </tr>
       </>
     )
   }
   const Statistics = () => {
     const countAll = bad + neutral + good
     const positive = (good / countAll * 100).toFixed(4)
-    const average = ((countAll) / 3).toFixed(4)+' %'
-    return(
-      <div>
-        <StatisticLine text="good" value ={good} />
-        <StatisticLine text="neutral" value ={neutral} />
-        <StatisticLine text="bad" value ={bad} />
-        <StatisticLine text="all" value ={parseFloat(countAll).toFixed(4)} />
-        <StatisticLine text="average" value ={average} />
-        <StatisticLine text="positive" value ={ isNaN(positive) ? 0 : positive} />
-      </div>
+    const average = ((countAll) / 3).toFixed(4) + ' %'
+    return (
+      <table>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={parseFloat(countAll).toFixed(4)} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={isNaN(positive) ? 0 : positive} />
+      </table>
     )
   }
   return (
@@ -45,10 +41,10 @@ function App() {
           setGood(good + 1)
         }}>good</button>
         <button onClick={() => {
-          setNeutral(neutral+1)
+          setNeutral(neutral + 1)
         }}>neutral</button>
         <button onClick={() => {
-          setBad(bad+1)
+          setBad(bad + 1)
         }}>bad</button>
       </div>
       <h2>statistic</h2>
